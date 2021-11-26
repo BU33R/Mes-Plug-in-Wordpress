53f8d8294d1b6af3178a7bd605c0e8701b350b94
B-U-3-R <87655366+BU33R@users.noreply.github.com>
2021-11-26 11:30:57 +0100

    Add files via upload

---
+++ Mon-plugin/mon-plugin.php
@@ -0,0 +1,16 @@
+<?php 
+
+/* 
+
+Plugin name: Plug-in Météo
+Description: Mon Plug-in Météo (PROJET)
+Plugin URI:
+Version: 1.0
+Tags: 
+Author: BU3R.
+Author URI: https://github.com/BU33R
+License: GPL3
+
+*/
+
+?>
