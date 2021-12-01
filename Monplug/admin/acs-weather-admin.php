08d3e16ce9ec971a479db7de6497877868aa0f16
B-U-3-R <87655366+BU33R@users.noreply.github.com>
2021-12-01 10:13:17 +0100

    Add files via upload

---
+++ Monplug/admin/acs-weather-admin.php
@@ -0,0 +1,19 @@
+
+
+<head>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/mon-css.css"/>
+</head>
+
+
+
+
+<div class="input-group-custom customText">
+
+    <span class="input-group-text">Clé API</span>
+
+    <input class="form-control" type="text" value="https://geo.api.gouv.fr/communes" aria-label="readonly input example" readonly>
+
+</div>
+
