62d4d9da40486d156b6db43c12665788b207f6a2
B-U-3-R <87655366+BU33R@users.noreply.github.com>
2021-12-01 09:42:15 +0100

    Add files via upload

---
+++ Monplug/acs-weather-info.php
@@ -0,0 +1,14 @@
+<?php
+
+/* -------------------------------------------------------------
+
+Plugin name: Plug'zz
+Description: Récupération et exploitation des données sur toutes les communes de France, mais aussi pour la météo.
+Version: 0.1
+Author: BU3R.
+Author URI:
+License: GPL3
+
+---------------------------------------------------------------- */
+
+require_once plugin_dir_path(__FILE__) . 'admin/config.php';
